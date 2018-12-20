d390d29ac6cff550ce9e33e41e3957d5c0011876
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-20 17:46:47 +0200

    fix test

--- src/main/java/com/icthh/xm/ms/dashboard/config/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/ms/dashboard/config/ApplicationProperties.java
@@ -3,7 +3,6 @@ package com.icthh.xm.ms.dashboard.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
