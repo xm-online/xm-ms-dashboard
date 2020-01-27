a0b4d7ddd520de5f173f306d8d58c5dac76707b9
anton.mostipan@softheme.com <anton.mostipan@softheme.com>
2020-01-27 20:20:27 +0200

    Updated gradle config to https links

--- src/main/java/com/icthh/xm/ms/dashboard/service/bulk/impl/BulkDashboardServiceImpl.java
+++ src/main/java/com/icthh/xm/ms/dashboard/service/bulk/impl/BulkDashboardServiceImpl.java
@@ -7,11 +7,12 @@ import com.icthh.xm.ms.dashboard.repository.DashboardRepository;
 import com.icthh.xm.ms.dashboard.service.bulk.BulkDashboardService;
 import com.icthh.xm.ms.dashboard.service.dto.BulkDashboardResult;
 import com.icthh.xm.ms.dashboard.service.dto.DashboardDto;
-import java.util.Collection;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
 
 @Slf4j
 @Service
