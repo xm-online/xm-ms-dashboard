0c2e2c92301d1c214aad3a4e84def3945b449aa3
Anatoliy Medvedchuk <anatoliy.medvedchuk@jevera.software>
2020-10-25 21:02:15 +0200

    increase commons verion

--- src/main/java/com/icthh/xm/ms/dashboard/config/tenant/TenantManagerConfiguration.java
+++ src/main/java/com/icthh/xm/ms/dashboard/config/tenant/TenantManagerConfiguration.java
@@ -1,6 +1,5 @@
 package com.icthh.xm.ms.dashboard.config.tenant;
 
-import com.icthh.xm.commons.exceptions.BusinessException;
 import com.icthh.xm.commons.migration.db.tenant.provisioner.TenantDatabaseProvisioner;
 import com.icthh.xm.commons.tenantendpoint.TenantManager;
 import com.icthh.xm.commons.tenantendpoint.provisioner.TenantAbilityCheckerProvisioner;
@@ -8,8 +7,6 @@ import com.icthh.xm.commons.tenantendpoint.provisioner.TenantListProvisioner;
 import com.icthh.xm.ms.dashboard.service.tenant.TenantDefaultDashboardProvisioner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
 
 @Slf4j
 @org.springframework.context.annotation.Configuration
@@ -26,21 +23,9 @@ public class TenantManagerConfiguration {
                                              .service(tenantListProvisioner)
                                              .service(databaseProvisioner)
                                              .service(dashboardProvisioner)
-                                             .exceptionHandler(defaultExceptionHandler())
                                              .build();
         log.info("Configured tenant manager: {}", manager);
         return manager;
-    }
-
-    // TODO remove after commons 2.0.17
-    private static <E extends Exception> Consumer<E> defaultExceptionHandler() {
-        return e -> {
-            if (e instanceof BusinessException) {
-                throw (BusinessException) e;
-            } else {
-                throw new BusinessException(e.getMessage());
-            }
-        };
     }
 
 }
