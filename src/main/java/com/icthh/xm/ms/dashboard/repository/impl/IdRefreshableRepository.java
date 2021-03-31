0cdcec88a830410424577ab47d884070f249adea
Serhii Senko <serhii.senko@jevera.software>
2021-03-31 12:56:14 +0300

    Fix counter

--- src/main/java/com/icthh/xm/ms/dashboard/repository/impl/IdRefreshableRepository.java
+++ src/main/java/com/icthh/xm/ms/dashboard/repository/impl/IdRefreshableRepository.java
@@ -2,6 +2,7 @@ package com.icthh.xm.ms.dashboard.repository.impl;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.icthh.xm.commons.config.client.api.RefreshableConfiguration;
@@ -11,11 +12,16 @@ import com.icthh.xm.commons.tenant.TenantContextUtils;
 import com.icthh.xm.ms.dashboard.config.ApplicationProperties;
 import com.icthh.xm.ms.dashboard.repository.IdRepository;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.AntPathMatcher;
 
@@ -24,7 +30,7 @@ import org.springframework.util.AntPathMatcher;
 @RequiredArgsConstructor
 public class IdRefreshableRepository implements RefreshableConfiguration, IdRepository {
 
-    private final Map<String, DashboardId> dashboards = new ConcurrentHashMap<>();
+    private final Map<String, DashboardCounterState> dashboards = new ConcurrentHashMap<>();
 
     private final AntPathMatcher matcher = new AntPathMatcher();
     private final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
@@ -43,8 +49,10 @@ public class IdRefreshableRepository implements RefreshableConfiguration, IdRepo
                 log.info("Specification for tenant {} was removed", tenant);
                 return;
             }
-            DashboardId spec = mapper.readValue(config, DashboardId.class);
-            dashboards.put(tenant, spec);
+            String oldHash = DigestUtils.sha1Hex(config);
+            DashboardIdDto dashboardIdDto = mapper.readValue(config, DashboardIdDto.class);
+            DashboardCounterState state = getDashboardCounterState(tenant);
+            state.globalCounterState.set(new DashboardGlobalCounterState(dashboardIdDto.getId(), oldHash));
             log.info("Specification was for tenant {} updated", tenant);
         } catch (Exception e) {
             log.error("Error read specification from path " + updatedKey, e);
@@ -66,27 +74,78 @@ public class IdRefreshableRepository implements RefreshableConfiguration, IdRepo
     @SneakyThrows
     public Long getNextId() {
         String tenant = getTenantKeyValue();
-        DashboardId dashboardId = dashboards.getOrDefault(tenant, new DashboardId());
-
-        if (dashboardId.getId() == null) {
-            dashboardId.setId(0L);
+        DashboardCounterState dashboardCounterState = getDashboardCounterState(tenant);
+        DashboardLocalCounter localCounter = dashboardCounterState.getLocalCounter();
+        Optional<Long> counterValue = localCounter.getNextCounter();
+        if (counterValue.isPresent()) {
+            return counterValue.get();
+        } else {
+            // acquire counter resource from ms config
+            DashboardLocalCounter dashboardLocalCounter = acquireCounterResource(tenant, 1000);
+            dashboardCounterState.localCounter.set(dashboardLocalCounter);
+            return dashboardLocalCounter.getNextCounter().orElseThrow(IllegalStateException::new);
         }
-        dashboardId.setId(dashboardId.getId() + 1);
-        dashboards.put(tenant, dashboardId);
-        tenantConfigRepository.updateConfigFullPath(tenant,
-                                            "/api"
-                                                    + applicationProperties.getTenantDashboardPropertiesIdPathPattern(),
-                                                    mapper.writeValueAsString(dashboardId));
+    }
 
-        return dashboardId.getId();
+    private DashboardLocalCounter acquireCounterResource(String tenant, long count) throws JsonProcessingException {
+        String path = "/api" + applicationProperties.getTenantDashboardPropertiesIdPathPattern();
+        DashboardGlobalCounterState dashboardCounterState = getDashboardCounterState(tenant).globalCounterState.get();
+        DashboardIdDto value = new DashboardIdDto();
+        value.setId(dashboardCounterState.currentValue + count);
+        String body = mapper.writeValueAsString(value);
+        tenantConfigRepository.updateConfigFullPath(tenant, path, body, dashboardCounterState.oldHash);
+        return new DashboardLocalCounter(value.getId(), count);
+    }
+
+    private DashboardCounterState getDashboardCounterState(String tenant) {
+        return dashboards.computeIfAbsent(tenant, key -> new DashboardCounterState());
     }
 
     private String getTenantKeyValue() {
         return TenantContextUtils.getRequiredTenantKeyValue(tenantContextHolder);
     }
 
+    public static class DashboardCounterState {
+        final AtomicReference<DashboardGlobalCounterState> globalCounterState = new AtomicReference<>(
+                new DashboardGlobalCounterState(0L, null)
+        );
+        final AtomicReference<DashboardLocalCounter> localCounter = new AtomicReference<>(
+                new DashboardLocalCounter(0L, 0L)
+        );
+
+        public DashboardLocalCounter getLocalCounter() {
+            return localCounter.get();
+        }
+    }
+
+    public static class DashboardLocalCounter {
+        final AtomicLong availableCounterResources;
+        final Long startCounterValue;
+        final Long counterResource;
+
+        public DashboardLocalCounter(Long startCounterValue, Long counterResource) {
+            this.availableCounterResources = new AtomicLong(counterResource);
+            this.startCounterValue = startCounterValue;
+            this.counterResource = counterResource;
+        }
+
+        public Optional<Long> getNextCounter() {
+            long currentValue = availableCounterResources.decrementAndGet();
+            if (currentValue == 0) {
+                return Optional.empty();
+            }
+            return Optional.of(startCounterValue + (counterResource - currentValue));
+        }
+    }
+
+    @RequiredArgsConstructor
+    public static class DashboardGlobalCounterState {
+        private final Long currentValue;
+        private final String oldHash;
+    }
+
     @Data
-    public static class DashboardId {
+    public static class DashboardIdDto {
         Long id;
     }
 
