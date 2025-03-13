779c8fe20c0adc0b4efbb09e049b5af073e41cb3
ssenko <serhii.senko@jevera.software>
2025-03-13 16:50:36 +0200

    fix spel evaluation

--- src/main/java/com/icthh/xm/ms/dashboard/config/XmSecurityConfiguration.java
+++ src/main/java/com/icthh/xm/ms/dashboard/config/XmSecurityConfiguration.java
@@ -1,9 +1,14 @@
 package com.icthh.xm.ms.dashboard.config;
 
+import com.icthh.xm.commons.permission.access.XmPermissionEvaluator;
 import com.icthh.xm.commons.security.jwt.TokenProvider;
 import com.icthh.xm.commons.security.spring.config.SecurityConfiguration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 
 @Configuration
 public class XmSecurityConfiguration extends SecurityConfiguration {
@@ -11,5 +16,13 @@ public class XmSecurityConfiguration extends SecurityConfiguration {
                                    @Value("${jhipster.security.content-security-policy}")
                                    String contentSecurityPolicy) {
         super(tokenProvider, contentSecurityPolicy);
+    }
+
+    @Primary
+    @Bean
+    static MethodSecurityExpressionHandler expressionHandler(XmPermissionEvaluator customPermissionEvaluator) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(customPermissionEvaluator);
+        return expressionHandler;
     }
 }
