5f342f5c02fc6f70a6967be857877f063f9f3fe1
vosiievska <valentinaosievskaya@gmail.com>
2025-07-03 10:52:30 +0300

    added lep context services initialisation

--- src/main/java/com/icthh/xm/ms/dashboard/config/lep/LepContextListener.java
+++ src/main/java/com/icthh/xm/ms/dashboard/config/lep/LepContextListener.java
@@ -3,6 +3,10 @@ package com.icthh.xm.ms.dashboard.config.lep;
 import com.icthh.xm.commons.lep.api.BaseLepContext;
 import com.icthh.xm.commons.lep.api.LepContextFactory;
 import com.icthh.xm.lep.api.LepMethod;
+import com.icthh.xm.ms.dashboard.service.DashboardService;
+import com.icthh.xm.ms.dashboard.service.ProfileService;
+import com.icthh.xm.ms.dashboard.service.UiDataService;
+import com.icthh.xm.ms.dashboard.service.WidgetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +14,28 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class LepContextListener implements LepContextFactory {
 
+    private final DashboardService dashboardService;
+    private final WidgetService widgetService;
+    private final UiDataService uiDataService;
+    private final ProfileService profileService;
+
     @Override
     public BaseLepContext buildLepContext(LepMethod lepMethod) {
-        return new LepContext();
+        LepContext lepContext = new LepContext();
+
+        lepContext.services = buildLepContextServices();
+
+        return lepContext;
+    }
+
+    private LepContext.LepServices buildLepContextServices() {
+        LepContext.LepServices lepServices = new LepContext.LepServices();
+
+        lepServices.dashboardService = dashboardService;
+        lepServices.widgetService = widgetService;
+        lepServices.uiDataService = uiDataService;
+        lepServices.profileService = profileService;
+
+        return lepServices;
     }
 }
