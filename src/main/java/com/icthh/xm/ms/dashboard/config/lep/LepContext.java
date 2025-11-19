02c719948f1bbd9de8f4bdef70424b8bd6f4afb0
vitalii.kyrychenko <vitalii.kyrychenko@jevera.software>
2025-11-19 11:59:19 +0200

    add tenant config service

--- src/main/java/com/icthh/xm/ms/dashboard/config/lep/LepContext.java
+++ src/main/java/com/icthh/xm/ms/dashboard/config/lep/LepContext.java
@@ -1,5 +1,6 @@
 package com.icthh.xm.ms.dashboard.config.lep;
 
+import com.icthh.xm.commons.config.client.service.TenantConfigService;
 import com.icthh.xm.commons.lep.api.BaseLepContext;
 import com.icthh.xm.commons.permission.service.PermissionCheckService;
 import com.icthh.xm.ms.dashboard.service.DashboardService;
@@ -19,6 +20,7 @@ public class LepContext extends BaseLepContext {
         public UiDataService uiDataService;
         public ProfileService profileService;
         public PermissionCheckService permissionService;
+        public TenantConfigService tenantConfigService;
     }
 
     public static class LepTemplates {
